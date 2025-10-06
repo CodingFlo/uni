a99abf7f981a8c00b5878e8960ff44cbc91b1ea3
codingFlo <florian05.kainrath@gmail.com>
2025-10-06 21:03:22 +0200

    Das PDF der Aufgaben von Übung1 hinzugefügt

--- eprog/Serie1/py.py
+++ eprog/Serie1/py.py
@@ -3,7 +3,7 @@ print("Hello, World! ", end="")
 print("I am Florian Kainrath and I study technical mathematics at TU Wien")
 
 # Übung 3
-# "" weglassen => Programm kann nicht aufgeführt werden, da der Text so als Befehle gehandhabt werden, anstelle eines Strings/Textes
+# "" weglassen => Programm kann nicht aufgeführt werden, da es so als Befehle gehandhabt werden, anstelle eines Strings/Textes
 
 # Übung 4
 grades = range(0, 101)  # 101 it only goes from 0 to n-1
@@ -11,5 +11,5 @@ grades = range(0, 101)  # 101 it only goes from 0 to n-1
 for grade in grades:
     # gewollte Ausgabe
     print("Your final grade is: ", grade)
-    # schönere Ausgabe mit Einheit
-    print("Your final grade is: " + grade + "%")
+    # formatierte Ausgabe mit Einheit
+    print("Your final grade is: {0}%".format(grade))

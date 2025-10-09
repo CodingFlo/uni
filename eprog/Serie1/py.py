5b92b17815c166cc19f178651dab44269cf9176c
codingFlo <florian05.kainrath@gmail.com>
2025-10-09 21:57:13 +0200

    EIMA Aufgabe als LATEX + Korrektur der EPROG Aufgabe 4

--- eprog/Serie1/py.py
+++ eprog/Serie1/py.py
@@ -6,10 +6,27 @@ print("I am Florian Kainrath and I study technical mathematics at TU Wien")
 # "" weglassen => Programm kann nicht aufgeführt werden, da es so als Befehle gehandhabt werden, anstelle eines Strings/Textes
 
 # Übung 4
-grades = range(0, 101)  # 101 it only goes from 0 to n-1
 
-for grade in grades:
-    # gewollte Ausgabe
-    print("Your final grade is: ", grade)
-    # formatierte Ausgabe mit Einheit
-    print("Your final grade is: {0}%".format(grade))
+
+def checkInput(text):
+    # Warnung: keine Überprüfung eingebaut, ob die Eingabe keine Zahl ist
+    userInput = float(input(text+":\t"))
+
+    if userInput > 100:
+        userInput = 100
+    elif userInput < 0:
+        userInput = 0
+
+    return userInput
+
+
+final_exam = checkInput("Final exam grade")
+weekly_exam = checkInput("wWekly exam grade")
+exercise = checkInput("Exercise grade")
+
+grade = 0.4 * final_exam + 0.4 * weekly_exam + 0.2 * exercise
+
+# gewollte Ausgabe
+print("Your final grade is:", grade)
+# formatierte Ausgabe mit Einheit
+print("Your final grade is: {0}%".format(grade))
